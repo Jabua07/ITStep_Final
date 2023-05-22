0a16c2a6640414cab36a48db044241d8f0282437
jabua07 <iraklijabua1111@gmail.com>
2023-05-22 18:01:40 +0400

    Register

---
+++ main.js
@@ -0,0 +1,34 @@
+ function toRegister() {
+ document.querySelector("#mainform").style.display="none"
+        
+        document.querySelector("#mainform1").style.display="flex"
+
+        document.getElementById("register").style.display="none"
+        document.getElementById("tologin").style.display="flex"
+    }
+
+    function toLogin() {
+        document.querySelector("#mainform").style.display="flex"
+        
+        document.querySelector("#mainform1").style.display="none"
+
+        document.getElementById("register").style.display="flex"
+        document.getElementById("tologin").style.display="none"
+      }
+
+    function register() {
+        let username = document.getElementById("username1").value
+        let firstname = document.getElementById("firstname1").value
+        let lastname = document.getElementById("lastname1").value
+        let email = document.getElementById("email1").value
+        let privatenum = document.getElementById("privatenumber1").value
+        let password = document.getElementById("password1").value;
+
+        fetch('http://kketelauri-001-site1.gtempurl.com/api/user/adduser',
+         { method: 'POST', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+          body: JSON.stringify({ "userName": username, "firstName": firstname, "lastName": lastname, "email": email, "privateNumber": privatenum, "password": password, "active": true })
+         }) .then(response => response.json()) .then(response => console.log(JSON.stringify(response)))
+
+document.getElementById("message").innerHTML="Registered Successfully!"
+      }
+
