0feb204208e6ad2a27a19a0f99247978c61ae8f4
jabua07 <iraklijabua1111@gmail.com>
2023-05-28 17:31:53 +0400

    add to cart (work in progress)

--- searchpage.js
+++ searchpage.js
@@ -11,11 +11,59 @@ function search() {
     }
   }
 
+  let products = [
+    {
+      "id": 1,
+      "itemName":"Cheese Burger",
+      "restoraunt":"Burger Arena",
+      "price":"$3.88",
+      "image":"Rectangle 336.png"
+  },
+
+  {
+      "id": 2,
+      "itemName":"Toffe's Cake",
+      "restoraunt":"Top Sticks",
+      "price":"$4.00",
+      "image":"Rectangle 336 (1).png"
+  },
+  {
+      "id": 3,
+      "itemName":"Dancake",
+      "restoraunt":"Cake World",
+      "price":"$1.99",
+      "image":"Rectangle 336 (2).png"
+  },
+  {
+      "id": 4,
+      "itemName":"Crispy Sandwitch",
+      "restoraunt":"Fastfood Dine",
+      "price":"$3.00",
+      "image":"Rectangle 336 (3).png"
+  },
+  {
+      "id": 5,
+      "itemName":"Thai  Soup",
+      "restoraunt":"Foody man",
+      "price":"$2.79",
+      "image":"Rectangle 336 (4).png"
+  },
+  {
+      "id": 6,
+      "itemName":"Cheese Burger",
+      "restoraunt":"Burger Arena",
+      "price":"$3.88",
+      "image":"Rectangle 336.png"
+  }
+  ]
+
 $("#detailsBurger").click(function () { 
   
   $("#items", ).css("display", "none");
   $("#itemsbottom", ).css("display", "none");
   $("#searchdiv").css("display","none")
+  $("#header").css("display","none")
+  $("#cart").css("display","none")
   $("#burgerd").css("display","flex")
 });
 
@@ -23,6 +71,8 @@ $(".X1").click(function () {
   $("#items", ).css("display", "flex");
   $("#itemsbottom", ).css("display", "flex");
   $("#searchdiv").css("display","flex")
+  $("#header").css("display","flex")
+  $("#cart").css("display","flex")
   $("#burgerd").css("display","none")
 });
 
@@ -31,6 +81,8 @@ $("#detailsToffe").click(function () {
   $("#items", ).css("display", "none");
   $("#itemsbottom", ).css("display", "none");
   $("#searchdiv").css("display","none")
+  $("#cart").css("display","none")
+  $("#header").css("display","none")
   $("#toffed").css("display","flex")
 });
 
@@ -39,6 +91,8 @@ $(".X2").click(function () {
   $("#itemsbottom", ).css("display", "flex");
   $("#searchdiv").css("display","flex")
   $("#toffed").css("display","none")
+  $("#header").css("display","flex")
+  $("#cart").css("display","flex")
 });
 
 $("#detailsCake").click(function () { 
@@ -46,6 +100,8 @@ $("#detailsCake").click(function () {
   $("#items", ).css("display", "none");
   $("#itemsbottom", ).css("display", "none");
   $("#searchdiv").css("display","none")
+  $("#cart").css("display","none")
+  $("#header").css("display","none")
   $("#dancaked").css("display","flex")
 });
 
@@ -54,6 +110,8 @@ $(".X3").click(function () {
   $("#itemsbottom", ).css("display", "flex");
   $("#searchdiv").css("display","flex")
   $("#dancaked").css("display","none")
+  $("#header").css("display","flex")
+  $("#cart").css("display","flex")
 });
 
 $("#detailsSandwich").click(function () { 
@@ -61,6 +119,8 @@ $("#detailsSandwich").click(function () {
   $("#items", ).css("display", "none");
   $("#itemsbottom", ).css("display", "none");
   $("#searchdiv").css("display","none")
+  $("#cart").css("display","none")
+  $("#header").css("display","none")
   $("#sandwichd").css("display","flex")
 });
 
@@ -69,6 +129,8 @@ $(".X4").click(function () {
   $("#itemsbottom", ).css("display", "flex");
   $("#searchdiv").css("display","flex")
   $("#sandwichd").css("display","none")
+  $("#header").css("display","flex")
+  $("#cart").css("display","flex")
 });
 
 $("#detailsSoup").click(function () { 
@@ -76,6 +138,8 @@ $("#detailsSoup").click(function () {
   $("#items", ).css("display", "none");
   $("#itemsbottom", ).css("display", "none");
   $("#searchdiv").css("display","none")
+  $("#header").css("display","none")
+  $("#cart").css("display","none")
   $("#soupd").css("display","flex")
 });
 
@@ -84,6 +148,8 @@ $(".X4").click(function () {
   $("#itemsbottom", ).css("display", "flex");
   $("#searchdiv").css("display","flex")
   $("#soupd").css("display","none")
+  $("#header").css("display","flex")
+  $("#cart").css("display","flex")
 });
 
 $("#foodwagon").click(function (e) { 
@@ -91,4 +157,36 @@ $("#foodwagon").click(function (e) {
   
 });
 
+let carts = document.querySelectorAll(".addToCart")
 
+for ( let i=0; i<carts.length; i++){
+carts[i].addEventListener('click', () => {
+
+  cartItems(products[i])
+})
+}
+
+
+
+
+
+function cartItems(product) {
+  let container1 = document.getElementById("itemcont")
+  let itemname1 = JSON.stringify(product.itemName)
+  let imagesrc1= JSON.stringify(product.image)
+  let newDiv = document.createElement('div')
+newDiv.innerHTML= `<div class="item"> 
+ <img width="15%" src=${imagesrc1} alt="">
+ <p>Item:${itemname1}</p>
+ <button onclick="removeButton()" id="remove">Remove </button>
+ 
+</div>`
+
+container1.appendChild(newDiv)
+
+
+  }
+
+  
+  
+  
